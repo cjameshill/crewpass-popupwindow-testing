05fb0d0ebb9eee73854284524b5a56af5acfea1d
Christopher Hill <cjameshill@gmail.com>
2022-08-31 18:03:07 +0200

    Setup vue and tailwind

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  </script>
+  
+  <template>
+    <header class="w-full p-8 flex justify-center items-center">
+      <div class="wrapper text-center">
+        <h1 class="text-xl font-medium mb-6">CrewPass Window Popup Testing</h1>
+        <nav class="flex space-x-4">
+        </nav>
+      </div>
+    </header>
+    <main class="w-full flex justify-center items-start p-6 min-h-[50vh] bg-gray-50">
+      <RouterView />
+    </main>
+  
+  </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from 'vue';
+const openWindow = () => {
+  window.open('http://127.0.0.1:5174', 'cpVesselPopup', 'width=700,height=800')
+}
+onMounted(() => {
+  window.addEventListener("message", (message) => {
+    console.log("message: ", message.data);
+  })
+})
+</script>
+<template>
+  <main class="w-4/5 flex flex-col justify-center items-center space-y-4">
+    <h1 class="text-lg font-medium">Open Popup Window and Message Testing</h1>
+    <button @click="openWindow" class="p-4 bg-gray-100 hover:bg-gray-300 rounded-xl">Open Window</button>
+  </main>
+</template>
