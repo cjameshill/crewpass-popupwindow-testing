8777b90635342afb889f81aa4a9d5a6523726cd4
Christopher Hill <cjameshill@gmail.com>
2022-09-01 08:02:33 +0000

    Added custom popup window input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,32 @@
 <script setup>
 import { onMounted, ref, computed } from 'vue';
-const popupUrl = ref("http://127.0.0.1:5173");
+const popupUrl = ref("http://127.0.0.1:5173?hello=world");
 const origin = window.location.origin;
 const popupFullUrl = computed(() => {
+  if (!popupUrl.value) return ""
   const url = new URL(popupUrl.value);
-  const fullUrl = new URL()
+  url.searchParams.append("origin", origin);
   return url.toString();
 })
 const openWindow = () => {
-  window.open(`${popupUrl.value}?origin=${origin}`, 'cpVesselPopup', 'width=700,height=800')
+  window.open(`popupFullUrl.value`, 'cpVesselPopup', 'width=700,height=800')
 }
 const messages = ref([]);
 onMounted(() => {
-  console.log("popupUrl: ", window.location);
   window.addEventListener("message", (message) => {
-    console.log("message: ", message);
     if (message.origin === popupUrl.value) {
+      console.log("message: ", message);
       messages.value.push(message.data);
     }
   })
 })
 </script>
 <template>
-  <main class="w-4/5 flex flex-col justify-center items-center space-y-6">
+  <main class="w-4/5 xl:w-1/2 flex flex-col justify-center items-center space-y-6">
     <h1 class="text-lg font-medium">Open Popup Window and Message Testing</h1>
-    <input v-model="popupUrl" type="text" class="p-4 rounded-xl border-2 border-gray-200" placeholder="URL" />
+    <input v-model="popupUrl" type="text" class="p-4 w-full rounded-xl border-2 border-gray-200" placeholder="URL" />
     <button @click="openWindow" class="p-4 bg-gray-100 hover:bg-gray-300 rounded-xl">Open Window</button>
-    <pre>{{ popupFullUrl }}</pre>
+    <p>{{ popupFullUrl }}</p>
     <h1 class="text-md mt-4">Messages:</h1>
     <pre v-for="message in messages">{{ message }}</pre>
   </main>
